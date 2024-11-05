e1e27184ab35310e676658d2ee71450f6a1c1977
unknown <farmerintech@gmail.com>
2024-11-05 16:20:15 +0100

    users routes, models, controllers added

--- middlewares/authoticator.js
+++ middlewares/authoticator.js
@@ -1,5 +1,6 @@
 export const Authenticator = (req, res, next) =>{
     const token = req.header("Authorization")?.replace("Bearer ", "");
+    console.log(req.header)
     if(!token){
         return res.status(401).json({msg:'not Authorized'})
     }
--- routes/booksRoute.js
+++ routes/booksRoute.js
@@ -8,14 +8,15 @@ import {
 } 
 from "../controllers/BooksController.js";
 import { Validator } from "../middlewares/validator.js";
+import { Authenticator } from "../middlewares/authoticator.js";
 
 const router = express.Router();
 //craete book
 router.post("/", Validator, createBook)
 // get all  books
-router.get("/", getBooks)
+router.get("/", Authenticator, getBooks)
 // get a single books
-router.get("/:id", getASingleBook)
+router.get("/:id", Authenticator, getASingleBook)
 // Update book
 router.put("/:id", Validator, updateBook)
 // delete book
